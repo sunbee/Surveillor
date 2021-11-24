680f482e5bac4b2242e3e516180240474d10b326
sunbee <sanjay.bhatikar@gmail.com>
2021-11-23 23:35:55 -0600

    Refactored notebook, expanded comments.

---
+++ ObjectDetector.py
@@ -0,0 +1,10 @@
+from tflite_runtime.interpreter import Interpreter
+from PIL import Image
+import numpy as np
+import time
+import os
+from picamera import PiCamera
+from io import BytesIO
+
+class ObjectDetector:
+    pass
